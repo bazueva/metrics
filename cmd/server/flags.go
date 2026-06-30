bf1a561982b81808c2618b9b9d6364fc7764f51b
Evgeniia <bazueva1988@gmail.com>
2026-06-30 18:26:28 +0500

    fix

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,12 +1,1 @@
 package main
-
-import (
-	"flag"
-
-	config2 "github.com/bazueva/metrics/cmd/config"
-)
-
-func parseFlags(config *config2.ServerAddr) {
-	flag.Var(config, "a", "address http server")
-	flag.Parse()
-}
