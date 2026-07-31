88d4a36d443d13bfac7c88ee342fb053c9ae5734
Evgeniia <bazueva1988@gmail.com>
2026-07-31 06:28:58 +0500

    fix migration

--- migrations/000001_create_metrics_table.up.sql
+++ migrations/000001_create_metrics_table.up.sql
@@ -10,7 +10,7 @@ create table IF NOT EXISTS metrics
         id SERIAL PRIMARY KEY,
         metric_id varchar(50) NOT NULL UNIQUE,
         type metrics_type NOT NULL,
-        delta integer,
+        delta bigint,
         value double precision,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
